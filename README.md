5765ab25103f9f51764070a82f76102e02fc8a69
Sivakumar Mopuri <ds.mopuri@gmail.com>
2024-11-27 16:59:55 +0530

    Changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-ML FLOW Experiements
+import dagshub
+dagshub.init(repo_owner='jobsmopuri', repo_name='mlflowexperiments', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- app.py
+++ app.py
@@ -65,8 +65,12 @@ if __name__=="__main__":
         
         predictions=lr.predict(train_x)
         signature=infer_signature(train_x,predictions)
+        remote_server_uri ="https://dagshub.com/jobsmopuri/mlflowexperiments.mlflow"
+        #mlflow.get_tracking_uri(remote_server_uri)
         
-        tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
+        
+        tracking_url_type_store=urlparse(mlflow.get_tracking_uri(remote_server_uri)).scheme
+        
         
         if tracking_url_type_store != "file":
             mlflow.sklearn.log_model(
